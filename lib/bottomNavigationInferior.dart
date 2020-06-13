c296eb4d21a6bfac8b1ba75bdb527ef77ed2a0db
cristianfchq <cristian.fchq@gmail.com>
2020-06-13 11:18:23 -0400

    firebase + flutter funcionando CRUD

--- lib/bottomNavigationInferior.dart
+++ lib/bottomNavigationInferior.dart
@@ -1,5 +1,5 @@
 
-import 'package:covid19/ui/views/bolivia/graphScreenBolivia.dart';
+import 'package:covid19/graphScreenBolivia.dart';
 import 'package:covid19/principalScreen.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
